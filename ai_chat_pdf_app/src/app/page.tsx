f6850547b3ef8cbf7e31d3f44b553733ab4f0604
Quito <quitoapps@gmail.com>
2024-06-28 20:20:21 +0000

    Create Next App

---
+++ ai_chat_pdf_app/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <h1 className="flex justify-center text-2xl text-lime-500">
+      This is Landing Page
+    </h1>
+  )
+}
